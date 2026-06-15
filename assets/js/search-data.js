1796bd06be895e977bd3fba9eb58f9975c2ca4c4
blakedewey <blake.dewey@jhu.edu>
2026-06-15 20:39:59 +0000

    Deploying to gh-pages from @ piksl-research/piksl-research.github.io@b0b72512857385a3f3ac402678581efac069c8e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
@@ -44,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "post-creating-an-mri-mega-dataset",
         
           title: "Creating an MRI Mega-Dataset",
@@ -55,14 +55,25 @@ ninja.data = [{
             window.location.href = "/blog/2025/mega-datasets/";
           
         },
-      },{id: "news-3d-mri-ddpm-project-release",
-          title: '3D MRI DDPM Project Release',
+      },{id: "news-3d-brain-mri-ddpm-project-release-preprint-code-and-pretrained-weights",
+          title: '3D Brain MRI DDPM Project Release: preprint, code, and pretrained weights',
+          description: "",
+          section: "News",},{id: "news-eclare-published-in-the-journal-of-medical-imaging",
+          title: 'ECLARE published in the Journal of Medical Imaging',
+          description: "",
+          section: "News",},{id: "news-new-project-page-single-image-super-resolution-for-clinical-mri",
+          title: 'New Project Page: Single-Image Super-Resolution for Clinical MRI',
           description: "",
           section: "News",},{id: "projects-ddpms-for-mri",
           title: 'DDPMs for MRI',
           description: "Diffusion-Driven Generation of Minimally Preprocessed Brain MRI",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ddpm-3dmri/";
+            },},{id: "projects-physics-based-single-image-super-resolution-for-mri",
+          title: 'Physics-Based Single-Image Super-Resolution for MRI',
+          description: "From self-supervised enhancement to generative-prior recovery",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/sisr/";
             },},{
         id: 'social-email',
         title: 'email',
